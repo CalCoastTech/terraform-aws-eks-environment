12a4a4c7b505edfbb263bddaf42a17558f1a1663
Bryan Karaffa <bryankaraffa@gmail.com>
2020-10-27 12:09:19 -0700

    add git tags, eks cluster_version, and vpc cidr

--- module_vpc.tf
+++ module_vpc.tf
@@ -1,4 +1,14 @@
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+
+  cidr = var.vpc_module_cidr
+
+  tags = module.git_metadata.all
+}
+
+variable "vpc_module_cidr" {
+  type = string
+  description = "The CIDR block for the VPC."
+  default = "10.0.0.0/16"
 }
